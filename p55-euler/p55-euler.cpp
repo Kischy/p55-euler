d075ad1d1a99b6ff7d114d2c05a30b57b7ea07cc
chaege_Thinkpad <c.haege@ipc.uni-stuttgart.de>
2019-11-22 17:01:02 +0100

    Added VS project and test project

---
+++ p55-euler/p55-euler.cpp
@@ -0,0 +1,18 @@
+// p55-euler.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <iostream>
+
+int main()
+{
+
+	unsigned long long p55_answ = 0;
+
+
+	std::cout << "The answer to the 55th problem of ProjectEuler.Net is:\n" << p55_answ << std::endl;
+
+
+	return 0;
+}
+
